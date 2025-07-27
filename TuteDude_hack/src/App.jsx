35fcada5eed76fa1558d2baf22196d2ab4cb9958
rohit-debnath24 <rohit.kumardebnath.cse28@heritageit.edu.in>
2025-07-27 09:22:07 +0530

    conflict resolved

--- TuteDude_hack/src/App.jsx
+++ TuteDude_hack/src/App.jsx
@@ -10,7 +10,7 @@ import Footer from './WebPages/Footer'
 import AuthPage from './Authentication/AuthPage'
 import ProfileSupplier from './WebPages/ProfileSupplier'
 import CategoryPage from './WebPages/Routes/CategoryPage'
-import AuthPage from './Authentication/AuthPage'
+//import AuthPage from './Authentication/AuthPage'
 
 
 
@@ -26,23 +26,9 @@ const App = () => {
   const [cartOpen, setCartOpen] = useState(false);
   return (
     <>
-<<<<<<< HEAD
-      <div>
-=======
     <div> 
-          <Nabvar/>
-   <HomePage/>
-   <ItemsListPage/>
-   <CategoryPage/>
+          
    
-   
-   <ItemsPage/>
-   <ProfilePage/>
-   <ProfileSupplier/>
-   <Footer/>
-   {/* <AuthPage/> */}
-   
->>>>>>> a83ba2bc5374429ad3cfd3c0678f6b9e822d215b
 
 
         <Cart open={cartOpen} onClose={() => setCartOpen(false)} />
@@ -51,9 +37,9 @@ const App = () => {
         <ItemsListPage />
         <CategoryPage />
 
-
         <ItemsPage />
         <ProfilePage />
+        <ProfileSupplier />
         <Footer />
         {/* <AuthPage/> */}
 
