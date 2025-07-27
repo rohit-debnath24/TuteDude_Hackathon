f62f2ec035e1c60b7ac32e9c715e1a4605777ddc
RitoDutta <ritodutta2006@gmail.com>
2025-07-27 09:43:27 +0530

    ok

--- TuteDude_hack/src/Authentication/AuthPage.jsx
+++ TuteDude_hack/src/Authentication/AuthPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AuthPage = () => {
   return (
-    <div className='w-1/2 h-[60%] z-30 fixed bg-violet-800 grid grid-rows-4 grid-cols-6 p-4  top-1/4 left-1/4  rounded-lg shadow-lg'>
+    <div className='hidden w-1/2 h-[60%] z-30 fixed bg-violet-800 grid grid-rows-4 grid-cols-6 p-4  top-1/4 left-1/4  rounded-lg shadow-lg'>
       <div className='row-span-1 col-span-4 bg-amber-300'>LOGO</div>
       <div className='row-span-1 col-span-4 bg-amber-300'>TITLE</div>
       <div className='row-span-1 col-span-4 bg-amber-300'>PHONE & BUTTON</div>
